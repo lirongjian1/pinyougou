a2141b3c837234284bc3f95f7e9c91b4d98b41a9
lironjian <1262140073@qq.com>
2020-03-24 14:28:33 +0800

    day2

--- pinyougou-service/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
+++ pinyougou-service/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -1,23 +1,100 @@
 package com.pinyougou.sellergoods.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
-import com.pinyougou.mapper.BrandMapper;
 import com.pinyougou.pojo.Brand;
+import com.pinyougou.mapper.BrandMapper;
 import com.pinyougou.service.BrandService;
+import java.util.List;
+import com.github.pagehelper.ISelect;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
+import tk.mybatis.mapper.entity.Example;
+import java.io.Serializable;
+import java.util.Arrays;
+/**
+ * BrandServiceImpl 服务接口实现类
+ * @date 2020-03-23 19:16:17
+ * @version 1.0
+ */
 @Service(interfaceName = "com.pinyougou.service.BrandService")
+/** 上面指定接口名，产生服务名，不然会用代理类的名称 */
 @Transactional
 public class BrandServiceImpl implements BrandService {
-    //第一天 为什么可以注入 看applicationContext-mapper.xml中生成代理对象配置
-    @Autowired
-    private BrandMapper brandMapper;
-   //第一天 查询所有品牌
-    @Override
-    public List<Brand> findAll() {
-        return brandMapper.findAll();
-    }
+
+	@Autowired
+	private BrandMapper brandMapper;
+
+	/** 添加方法 */
+	public void save(Brand brand){
+
+			brandMapper.insertSelective(brand);
+	}
+
+	/** 修改方法 */
+	public void update(Brand brand){
+
+			brandMapper.updateByPrimaryKeySelective(brand);
+	}
+
+	/** 根据主键id删除 */
+	public void delete(Serializable id){
+		try {
+			brandMapper.deleteByPrimaryKey(id);
+		}catch (Exception ex){
+			throw new RuntimeException(ex);
+		}
+	}
+
+	/** 批量删除 */
+	public void deleteAll(Serializable[] ids){
+		try {
+			// 创建示范对象
+			Example example = new Example(Brand.class);
+			// 创建条件对象
+			Example.Criteria criteria = example.createCriteria();
+			// 创建In条件
+			criteria.andIn("id", Arrays.asList(ids));
+			// 根据示范对象删除
+			brandMapper.deleteByExample(example);
+		}catch (Exception ex){
+			throw new RuntimeException(ex);
+		}
+	}
+
+	/** 根据主键id查询 */
+	public Brand findOne(Serializable id){
+		try {
+			return brandMapper.selectByPrimaryKey(id);
+		}catch (Exception ex){
+			throw new RuntimeException(ex);
+		}
+	}
+
+	/** 查询全部 */
+	public List<Brand> findAll(){
+		try {
+			return brandMapper.selectAll();
+		}catch (Exception ex){
+			throw new RuntimeException(ex);
+		}
+	}
+
+	/** 多条件分页查询 */
+	public List<Brand> findByPage(Brand brand, int page, int rows){
+		try {
+			PageInfo<Brand> pageInfo = PageHelper.startPage(page, rows)
+				.doSelectPageInfo(new ISelect() {
+					@Override
+					public void doSelect() {
+						brandMapper.selectAll();
+					}
+				});
+			return pageInfo.getList();
+		}catch (Exception ex){
+			throw new RuntimeException(ex);
+		}
+	}
+
 }
